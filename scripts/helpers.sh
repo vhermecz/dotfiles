4946f2c747b3820ae6aec0db3e4723f201829e9d
John Jeffers <john@jeffers.cc>
2023-07-27 21:14:52 -0600

    update helper functions

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Set some colors
 C_GRAY=$(tput setaf 248)
 C_GREEN=$(tput setaf 2)
 C_RED=$(tput setaf 202)
@@ -7,20 +8,15 @@ C_RESET=$(tput sgr0)
 C_YELLOW=$(tput setaf 228)
 export C_GRAY C_GREEN C_RED C_RESET C_YELLOW
 
+# Logging functions
 error()   { echo -e "${C_RED}${1}${C_RESET}" >&2; }
 success() { echo -e "${C_GREEN}${1}${C_RESET}"; }
 info()    { echo -e "${C_GRAY}${1}${C_RESET}"; }
 
+# Backup a file, use timestamp as extension
 backup_file() { mv "${1}" "${1}.$(date '+%Y%m%d%H%M%S')"; }
 
-dir_exists() {
-    if ! [[ -d "${1}" ]]; then return 1; fi
-}
-
-file_exists() {
-    if ! [[ -f "${1}" ]]; then return 1; fi
-}
-
-is_symlink() {
-    if ! [[ -L "${1}" ]]; then return 1; fi
-}
+# "Does x exist" functions
+dir_exists()  { if ! [[ -d "${1}" ]]; then return 1; fi }
+file_exists() { if ! [[ -f "${1}" ]]; then return 1; fi }
+is_symlink()  { if ! [[ -L "${1}" ]]; then return 1; fi }
